92b80ec296f40a92e67535ea79d532710238771b
Brandon Beck <beckbran@gmail.com>
2021-01-03 23:08:55 -0600

    started group membership coding

--- users.go
+++ users.go
@@ -128,3 +128,34 @@ func (c *Client) SetUserConnectionPermissions(username string, permissionItems *
 	}
 	return nil
 }
+
+// SetUserGroupMembership defines parent groups of a given group
+func (c *Client) SetUserGroupMembership(username string, permissionItems *[]types.GuacPermissionItem) error {
+	request, err := c.CreateJSONRequest(http.MethodPatch, fmt.Sprintf("%s/%s/%s/userGroups", c.baseURL, userGroupsBasePath, username), permissionItems)
+
+	if err != nil {
+		return err
+	}
+
+	err = c.Call(request, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetUserGroupMembership defines parent groups of a given group
+func (c *Client) GetUserGroupMembership(username string, permissionItems *[]types.GuacPermissionItem) ([]string, error) {
+	var ret []string
+	request, err := c.CreateJSONRequest(http.MethodGet, fmt.Sprintf("%s/%s/%s/userGroups", c.baseURL, userGroupsBasePath, username), nil)
+
+	if err != nil {
+		return ret, err
+	}
+
+	err = c.Call(request, &ret)
+	if err != nil {
+		return ret, err
+	}
+	return ret, nil
+}
